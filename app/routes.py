4337b44e088bca724def1f78c01020cf1a7a980f
Luidy-Michael <luidymichael8@gmail.com>
2025-06-15 22:39:39 -0300

    ajustes de erros e função p/ remover tarefas

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from app import app, database
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from app.models import Tarefa, ImportanciaEnum
 from app.forms import FormCriarTarefa
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    todas_tarefas = Tarefa.query.all()
+    return render_template('home.html', todas_tarefas=todas_tarefas)
 
 
 @app.route('/adicionar_tarefas', methods=["POST", "GET"])
@@ -24,6 +25,19 @@ def adicionar_tarefas():
         print('deu certo!!!!!!!! eu acho :) :>')
         return redirect(url_for('home'))
     else:
-        print('deu errado!!!!!! :( :<')
+        print('Erro no formulário: ')
+        print(form_nova_tarefa.errors)
 
     return render_template('adicionar_tarefas.html', form_nova_tarefa=form_nova_tarefa)
+
+
+
+@app.route('/remover_tarefa/<int:id_tarefa>', methods=["POST"])
+def remover_tarefa(id_tarefa):
+    tarefa_removida = Tarefa.query.get_or_404(id_tarefa)
+    if tarefa_removida:
+        database.session.delete(tarefa_removida)
+        database.session.commit()
+        return redirect(url_for('home'))
+    else:
+        return flash('Erro ao buscar tarefa!')
