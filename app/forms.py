65c5a901690dee26676234452e71fdaff1e91781
Luidy-Michael <luidymichael8@gmail.com>
2025-06-17 20:36:46 -0300

    adicionada verificação de email e senha

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateTimeLocalField, SelectField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, ValidationError, Email, EqualTo
 from app.models import ImportanciaEnum
-from app.models import Tarefa
-
+from app.models import Tarefa, Usuario
+import re
 
 class FormTarefa(FlaskForm):
     titulo_tarefa = StringField('Título da Tarefa: ', validators=[DataRequired(), Length(1, 30)])
@@ -19,8 +19,34 @@ class FormCriarConta(FlaskForm):
     confirmacao_senha = PasswordField('Confirme a senha', validators=[DataRequired(), EqualTo('senha')])
     btn_submit_criar_conta = SubmitField('Criar Conta')
 
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError('E-mail já cadastrado!')
+
+    def validate_senha(self, senha):
+        valor_senha = senha.data
+        if not re.search(r'\d', valor_senha):
+            raise ValidationError('A senha deve conter pelo menos um número!')
+        elif not re.search(r'[a-z]', valor_senha):
+            raise ValidationError('A senha deve conter pelo menos uma letra minúscula!')
+        elif not re.search(r'[A-Z]', valor_senha):
+            raise ValidationError('A senha deve conter pelo menos uma letra maiúscula!')
+        elif not re.search(r'[!@#$%^&*(),.?":{}	<>]', valor_senha):
+            raise ValidationError('A senha deve conter pelo menos um caracter especial!')
+
+
 
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
     btn_submit_login = SubmitField('Login')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if not usuario:
+            raise ValidationError(f'Nenhuma conta associada ao e-mail: {email.data}')
+
+
+
+
